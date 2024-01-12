2aaf292240a608e1e734751d7c0940835a8eb6fe
Nitin-U <nitinutsav80@gmail.com>
2024-01-12 19:22:34 +0545

    One to One model for UserData which contains additional fields(attributes) for User table

--- Dharma_App/login/models.py
+++ Dharma_App/login/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
+class UserData(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=10, default='')
+    USER_CHOICES = [
+        ('Cust', 'Customer'),
+        ('Pack', 'Package Provider'),
+        ('Prod', 'Product Seller'),
+    ]
+    role = models.CharField(max_length=20, choices=USER_CHOICES, default='')

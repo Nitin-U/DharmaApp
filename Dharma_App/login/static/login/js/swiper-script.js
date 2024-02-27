d8e8d1d25212ca83edd72b5bc3e3e6a7a74afe53
Nitin-U <nitinutsav80@gmail.com>
2024-02-27 16:59:02 +0545

    Added package and services section on homepage. Using Swiper JS library.

---
+++ Dharma_App/login/static/login/js/swiper-script.js
@@ -0,0 +1,36 @@
+var swiper = new Swiper(".slide-container-custom", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  sliderPerGroup: 4,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1.25,
+    },
+    576: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    992: {
+      slidesPerView: 3,
+    },
+    1200: {
+      slidesPerView: 4,
+    },
+  },
+});

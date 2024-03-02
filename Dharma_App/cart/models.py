76ac14ce884ad59740596fa772a918fcef2276c2
Nitin-U <nitinutsav80@gmail.com>
2024-03-02 13:41:58 +0545

    Seller's Product List Page Added

--- Dharma_App/cart/models.py
+++ Dharma_App/cart/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+#
+# Product Model for Items to buy/sell
+class Product(models.Model):
+    seller = models.ForeignKey(User, on_delete=models.CASCADE)
+    product_name = models.CharField(max_length=144)
+    PRODUCT_TYPE_CHOICES = [
+        ('', '--- Your Product Falls Under Which Type? ---'),
+        ('Tour Package', 'Tour Package'),
+        ('Product Items', 'Product Items'),
+    ]
+    product_type = models.CharField(max_length=144, choices=PRODUCT_TYPE_CHOICES)
+    PRODUCT_CATEGORY_CHOICES = [
+        ('', '--- Your Product Falls Under Which Type? ---'),
+        ('Category1', 'Category1'),
+        ('Category2', 'Category2'),
+        ('Category3', 'Category3'),
+        ('Category4', 'Category4'),
+        ('Category5', 'Category5'),
+    ]
+    product_category = models.TextField(max_length=144, choices=PRODUCT_CATEGORY_CHOICES)
+    product_description = models.CharField(max_length=144)
+    product_quantity = models.IntegerField()
+    product_price = models.DecimalField(max_digits=10, decimal_places=2)
+    product_stock = models.BooleanField()
+
+    def __str__(self):
+        return self.product_name
--- Dharma_App/cart/views.py
+++ Dharma_App/cart/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import User, Product
 
 # Create your views here.
 def productlist(request):
-    return render (request, 'cart/productlist.html')
+    current_user = request.user  # Since we are using Django's built-in authentication system, we request the current user associated to product
+
+    products = Product.objects.filter(seller=current_user) # User associated to their particular product
+
+    return render (request, 'cart/productlist.html', {'products': products, 'user': current_user})
